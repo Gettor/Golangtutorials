aacd40431e7d7388518ff0b07eb075574812fa6c
Gettor <sdobroc@gmail.com>
2025-01-25 17:20:47 +0100

    calculator done

--- calculator/main.go
+++ calculator/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"strconv"
@@ -25,6 +26,9 @@ func main() {
 	})
 
 	e.GET("/add/:first/:second", addition)
+	e.GET("/sub/:first/:second", subtraction)
+	e.GET("/mult/:first/:second", multiplication)
+	e.GET("/div/:first/:second", division)
 
 	httpPort := os.Getenv("PORT")
 	if httpPort == "" {
@@ -44,4 +48,63 @@ func addition(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, "Second argument is not a number.")
 	}
 	return c.String(http.StatusOK, strconv.FormatInt(int64(a+b), 10))
+}
+
+func subtraction(c echo.Context) error {
+	a, err := strconv.ParseInt(c.Param("first"), 10, 64)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "First argument is not a number.")
+	}
+	b, err := strconv.ParseInt(c.Param("second"), 10, 64)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Second argument is not a number.")
+	}
+	return c.String(http.StatusOK, strconv.FormatInt(int64(a-b), 10))
+}
+
+func multiply(left, right int64) (int64, error) {
+	const mostPositive = 1<<63 - 1
+	const mostNegative = -(mostPositive + 1)
+	result := left * right
+	if left == 0 || right == 0 || left == 1 || right == 1 {
+		return result, nil
+	}
+	if left == mostNegative || right == mostNegative {
+		return result, fmt.Errorf("overflow multiplying %v and %v", left, right)
+	}
+	if result/right != left {
+		return result, fmt.Errorf("overflow multiplying %v and %v", left, right)
+	}
+	return result, nil
+}
+
+func multiplication(c echo.Context) error {
+	a, err := strconv.ParseInt(c.Param("first"), 10, 64)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "First argument is not a number.")
+	}
+	b, err := strconv.ParseInt(c.Param("second"), 10, 64)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Second argument is not a number.")
+	}
+	result, err := multiply(a, b)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err)
+	}
+	return c.String(http.StatusOK, strconv.FormatInt(int64(result), 10))
+}
+
+func division(c echo.Context) error {
+	a, err := strconv.ParseInt(c.Param("first"), 10, 64)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "First argument is not a number.")
+	}
+	b, err := strconv.ParseInt(c.Param("second"), 10, 64)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Second argument is not a number.")
+	}
+	if b == 0 {
+		return echo.NewHTTPError(http.StatusBadRequest, "Division by zero detected")
+	}
+	return c.String(http.StatusOK, strconv.FormatFloat(float64(float64(a)/float64(b)), 'f', 2, 64))
 }
